43e6c075ccf19edd2c5fe4099626771bb1425184
rohithkumar004 <vankayalapatirohith0@gmail.com>
2025-08-07 11:54:00 +0530

    Update Aboutus.jsx

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -5,7 +5,8 @@ import { Link, Outlet } from "react-router-dom";
 const AboutUs = () => {
   return (
     <div className="about-container">
-      <h2>About Our Electronic Store</h2>
+      <h2>About Our Electronic Store in vijayawada</h2>
+                  <h3>V Rohith Kumar</h3>
       <p>
         Welcome to our E-Commerce platform, your one-stop shop for the latest and greatest in electronics!
         We specialize in high-quality products, including computers, mobiles, laptops, and accessories.
